912ab2801488794ffed9cf2c7554f3e089a5e892
Gijs Koehorst <gijskoehorst@hotmail.com>
2023-02-16 16:52:33 +0100

    Download punkt from nltk

--- process_sql.py
+++ process_sql.py
@@ -26,7 +26,10 @@
 
 import json
 import sqlite3
+import nltk
 from nltk import word_tokenize
+
+nltk.download('punkt')
 
 CLAUSE_KEYWORDS = ('select', 'from', 'where', 'group', 'order', 'limit', 'intersect', 'union', 'except')
 JOIN_KEYWORDS = ('join', 'on', 'as')
